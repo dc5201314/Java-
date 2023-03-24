1a9fffb339f17a22f10dad05569f71e5437e3d73
dc <1090504489@qq.com>
2023-03-24 17:05:42 +0800

    包装类

---
+++ src/main/java/top/dc/packing/WrapperClassDemo2.java
@@ -0,0 +1,15 @@
+package top.dc.packing;
+
+public class WrapperClassDemo2 {
+    public static void main(String[] args) {
+        // 自动装箱
+        int num1 = 19;
+        Integer num2 = num1;
+        System.out.println("num2=" + num2);
+
+        // 手动装箱:在 Java14 中已经过时，不推荐这样的操作
+        Integer num3 = new Integer(20);
+        System.out.println("num3=" + num3);
+    }
+
+}
