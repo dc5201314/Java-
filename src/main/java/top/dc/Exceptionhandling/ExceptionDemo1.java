d40ecdbcafe0b708b1b0d23c8cfa3d80bb8fc279
dc <1090504489@qq.com>
2023-03-24 16:38:32 +0800

    捕获异常

---
+++ src/main/java/top/dc/Exceptionhandling/ExceptionDemo1.java
@@ -0,0 +1,12 @@
+package top.dc.Exceptionhandling;
+
+public class ExceptionDemo1 {
+    public static void divide(int a, int b) {
+        System.out.println(a / b);
+    }
+
+    public static void main(String[] args) {
+        // 调用 divide() 方法
+        divide(2, 0);
+    }
+}
